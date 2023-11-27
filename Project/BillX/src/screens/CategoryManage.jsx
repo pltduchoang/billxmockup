7bb85bd28ccf29a6e4485d9f8e0e40e2fb763bd0
Kiydo <e.magtoto@hotmail.com>
2023-11-27 01:37:47 -0700

    category overview

--- Project/BillX/src/screens/CategoryManage.jsx
+++ Project/BillX/src/screens/CategoryManage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import Test from '../screens/CategoryOverview';
 
 function CategoryMange() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome to CategoryMange!</Text>
       {/* Add your content for the Home Month View */}
+      <Test />
     </View>
   );
 }
